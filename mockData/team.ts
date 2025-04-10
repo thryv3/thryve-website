437423ab2ac4058ee87e13ef5ff9099691e9cc90
fuentes-vincent <fuentes.vincent96@gmail.com>
2025-04-10 18:28:02 +0800

    Added profile

--- mockData/team.ts
+++ mockData/team.ts
@@ -6,8 +6,8 @@ export interface TeamMember {
 }
 
 export const teamData: TeamMember[] = [
-    { name: "Gnash Oppa Chi", role: "Developer", imageUrl: "/images/john.png" },
-    { name: "Vincent Papi Cholo", role: "Machu Dancer", imageUrl: "/images/john.png" },
+    { name: "Gnash Oppa Chi", role: "Developer", imageUrl: "/images/gnash-profile.jpg" },
+    { name: "Vincent Papi Cholo", role: "Machu Dancer", imageUrl: "/images/vincent-profile.png" },
     { name: "Mike Chen", role: "Mobile Developer", imageUrl: "/images/john.png" },
     { name: "Emma Davis", role: "Project Manager", imageUrl: "/images/john.png" },
 ] 
